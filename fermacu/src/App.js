392b2b447a3a4ba79d59df729bdd3fcfa8e1c9a1
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-15 00:11:29 -0600

    refactor: improving of App📦

--- fermacu/src/App.js
+++ fermacu/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { theme } from "./theme";
+
+//UI Material
+import { ThemeProvider } from "@mui/material";
+
+//React Router Dom
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Components
+import Appviewport from "./Components/Appviewport";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Appviewport/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
