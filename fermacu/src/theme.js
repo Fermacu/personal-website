657d6b6f2e2c8c53784c0ad95f8ea71487f76bf5
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-15 00:13:42 -0600

    feat: theme file✨

---
+++ fermacu/src/theme.js
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material";
+
+//Fonts
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/700.css";
+
+export const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#FFF",
+    },
+    secondary: {
+      main: "#FD4600",
+    },
+  },
+  typography: {
+    fontFamily: "Montserrat",
+    button: {
+      fontWeight: 900,
+    },
+  },
+  shape: {
+    borderRadius: 35,
+  },
+});
