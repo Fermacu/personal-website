56cee426ad0b235552acdc020bfa52b26f0bcde6
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-16 01:47:21 -0600

    refactor: improving of EducationSection📦

--- fermacu/src/Components/EducationSection.jsx
+++ fermacu/src/Components/EducationSection.jsx
@@ -15,23 +15,23 @@ export default function EducationSection() {
     <Grid container spacing={2} sx={{ color: "white", marginTop: "10px" }}>
       <Grid item xs={12}>
         <Stack direction="row" alignItems="center" spacing={2}>
-          <Business color="secondary"/>
+          <Business color="secondary" />
           <Stack>
-            {typographyForText("Universidad de Costa Rica")}
             {typographyForText(
               "Superior Technician in Business Administration",
               "bold"
             )}
+            {typographyForText("Universidad de Costa Rica")}
             {typographyForText("2021-2022", "bold")}
           </Stack>
         </Stack>
       </Grid>
       <Grid item xs={12}>
         <Stack direction="row" alignItems="center" spacing={2}>
-          <Phonelink color="secondary"/>
+          <Phonelink color="secondary" />
           <Stack>
+            {typographyForText("Technician in Software Development", "bold")}{" "}
             {typographyForText("Universidad Cenfotec")}
-            {typographyForText("Technician in Software Development", "bold")}
             {typographyForText("2022-2023", "bold")}
           </Stack>
         </Stack>
@@ -41,9 +41,6 @@ export default function EducationSection() {
           size="small"
           variant="outlined"
           startIcon={<School />}
-          onClick={() => {
-            window.location.href = "https://github.com/Fermacu";
-          }}
           endIcon={<ArrowForwardIosRounded />}
         >
           View more
