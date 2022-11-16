6ad8da85eb365b12bfe31c69cc51c5f35b1ad6cb
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-16 01:46:57 -0600

    refactor: improving of AboutMeCard📦

--- fermacu/src/Components/AboutMeCard.jsx
+++ fermacu/src/Components/AboutMeCard.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 
 //UI Material
-import { Box, Grid, Tab, Tabs } from "@mui/material";
+import { Box, Grid, Stack, Tab, Tabs } from "@mui/material";
 
 //Helpers
-import { typographyForText } from "../Helpers/typography";
+import {
+  typographyForMainTitles,
+  typographyForText,
+} from "../Helpers/typography";
 import EducationSection from "./EducationSection";
+import ExperienceSection from "./ExperienceSection";
 
 export default function AboutMeCard() {
   // ==========> Component States <==========
@@ -38,34 +42,58 @@ export default function AboutMeCard() {
     };
   }
   return (
-    <Grid
-      container
-      sx={{
-        padding: "20px",
-        backgroundColor: "#0D1117",
-        borderRadius: "20px",
-      }}
-    >
+    <Grid container spacing={2} justifyContent="flex-start">
       <Grid item xs={12}>
-        <Box>
-          <Tabs
-            centered
-            indicatorColor="secondary"
-            value={tabValue}
-            onChange={handleChange}
-            aria-label="actions"
-            textColor="secondary"
-          >
-            <Tab sx={{ color: "white" }} label="Education" {...a11yProps(0)} />
-            <Tab sx={{ color: "white" }} label="Experience" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
+        <Stack justifyContent="flex-start">
+          {typographyForMainTitles("I am Fernando Matamoros", "white")}
+          {typographyForText(
+            "I'm Fernando, a professional web designer & front end developer focused on crafting clean & user friendly experiences.",
+            null,
+            "white"
+          )}
+        </Stack>
       </Grid>
       <Grid item xs={12}>
-        <TabPanel value={tabValue} index={0}>
-          <EducationSection />
-        </TabPanel>
-        <TabPanel value={tabValue} index={1}></TabPanel>
+        <Grid
+          container
+          sx={{
+            padding: "20px",
+            backgroundColor: "#0D1117",
+            borderRadius: "20px",
+          }}
+        >
+          <Grid item xs={12}>
+            <Box>
+              <Tabs
+                centered
+                indicatorColor="secondary"
+                value={tabValue}
+                onChange={handleChange}
+                aria-label="actions"
+                textColor="secondary"
+              >
+                <Tab
+                  sx={{ color: "white" }}
+                  label="Experience"
+                  {...a11yProps(0)}
+                />
+                <Tab
+                  sx={{ color: "white" }}
+                  label="Education"
+                  {...a11yProps(1)}
+                />
+              </Tabs>
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <TabPanel value={tabValue} index={0}>
+              <ExperienceSection />
+            </TabPanel>
+            <TabPanel value={tabValue} index={1}>
+              <EducationSection />
+            </TabPanel>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
