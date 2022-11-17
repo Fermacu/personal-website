9b1e680cdb1e83d1b76f743c907b27d294f9ad53
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-17 01:03:05 -0600

    refactor: improving of EducationSection component📦

--- fermacu/src/Components/EducationSection.jsx
+++ fermacu/src/Components/EducationSection.jsx
@@ -3,8 +3,10 @@ import React from "react";
 //UI Material
 import { Button, Grid, Stack } from "@mui/material";
 import {
+  AppShortcut,
   ArrowForwardIosRounded,
   Business,
+  Code,
   Phonelink,
   School,
 } from "@mui/icons-material";
@@ -13,6 +15,9 @@ import { typographyForText } from "../Helpers/typography";
 export default function EducationSection() {
   return (
     <Grid container spacing={2} sx={{ color: "white", marginTop: "10px" }}>
+      <Grid item xs={12}>
+        {typographyForText("> College", "bold")}
+      </Grid>
       <Grid item xs={12}>
         <Stack direction="row" alignItems="center" spacing={2}>
           <Business color="secondary" />
@@ -33,6 +38,27 @@ export default function EducationSection() {
             {typographyForText("Technician in Software Development", "bold")}{" "}
             {typographyForText("Universidad Cenfotec")}
             {typographyForText("2022-2023", "bold")}
+          </Stack>
+        </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        {typographyForText("> Courses", "bold")}
+      </Grid>
+      <Grid item xs={12}>
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <AppShortcut color="secondary" />
+          <Stack>
+            {typographyForText("React Native: Apps for iOS & Android", "bold")}{" "}
+            {typographyForText("Udemy")}
+          </Stack>
+        </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <Code color="secondary" />
+          <Stack>
+            {typographyForText("JavaScript Essentials", "bold")}{" "}
+            {typographyForText("Udemy")}
           </Stack>
         </Stack>
       </Grid>
