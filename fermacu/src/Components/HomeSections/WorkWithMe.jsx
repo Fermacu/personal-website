3a15d61e24d31117826bac5f321c5254e16e0b69
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-17 20:55:36 -0600

    refactor: WorkWithMe component📦

--- fermacu/src/Components/HomeSections/WorkWithMe.jsx
+++ fermacu/src/Components/HomeSections/WorkWithMe.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
 //UI Material
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import { ArrowForwardIos, Email } from "@mui/icons-material";
 
 //Helpers
 import {
   typographyForMainTitles,
-} from "../Helpers/typography";
+} from "../../Helpers/typography";
 
 export default function WorkWithMeCard() {
   return (
     <Grid container spacing={2} justifyContent="flex-start">
       <Grid item xs={12}>
         {typographyForMainTitles("Think. Design. Develop.", "white")}
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="secondary" startIcon={<Email/>} endIcon={<ArrowForwardIos/>}>
+          LET'S TALK
+        </Button>
       </Grid>
     </Grid>
   );
