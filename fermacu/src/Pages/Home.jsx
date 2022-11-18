f44bb9c038db529f10e94b7661d32131c93f5389
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-17 20:54:41 -0600

    refactor: Home page improvement📦

--- fermacu/src/Pages/Home.jsx
+++ fermacu/src/Pages/Home.jsx
@@ -4,51 +4,44 @@ import React from "react";
 import { Grid } from "@mui/material";
 
 //Components
-import ActionsCard from "../Components/ActionsCard";
-import AboutMeCard from "../Components/AboutMeCard";
+import Dashboard from "../Components/HomeSections/Dashboard";
+import AboutMe from "../Components/HomeSections/AboutMe";
+import WorkWithMe from "../Components/HomeSections/WorkWithMe";
 
 //Helpers
 import {
-  typographyForMainTitles,
   typographyForSubtitles,
 } from "../Helpers/typography";
-import WorkWithMeCard from "../Components/WorkWithMeCard";
 
 export default function Home() {
   // ==========> Component <==========
   return (
     <Grid container spacing={1} justifyContent="center">
+
       {/**SECTION 1 - HOME */}
       <Grid item xs={12} md={10}>
         {typographyForSubtitles("> Home", "secondary")}
       </Grid>
       <Grid item xs={12} md={10}>
-        {typographyForMainTitles(
-          "The place where coffee becomes code.",
-          "white"
-        )}
+        <Dashboard />
       </Grid>
-      <Grid item xs={12} md={10}>
-        <ActionsCard />
-      </Grid>
+
       {/**SECTION 2 - ABOUT ME */}
       <Grid item xs={12} md={10} sx={{marginTop:"20px"}}>
         {typographyForSubtitles("> About Me", "secondary")}
       </Grid>
       <Grid item xs={12} md={10}>
-        <AboutMeCard/>
+        <AboutMe/>
       </Grid>
-      {/**SECTION 3 - HIRE ME*/}
+
+      {/**SECTION 3 - WORK WITH ME*/}
       <Grid item xs={12} md={10} sx={{marginTop:"20px"}}>
         {typographyForSubtitles("> Have An Idea? Let's Build It.", "secondary")}
       </Grid>
       <Grid item xs={12} md={10}>
-        <WorkWithMeCard/>
+        <WorkWithMe/>
       </Grid>
-      {/**SECTION 4 - CONTACT ME*/}
-      <Grid item xs={12} md={10} sx={{marginTop:"20px"}}>
-        {typographyForSubtitles("> Any Type Of Query & Discussion", "secondary")}
-      </Grid>
+      
     </Grid>
   );
 }
