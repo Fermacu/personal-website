a00a98e0120e5874d4a65ef01e47c77f393dedbd
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-26 02:07:19 -0600

    refactor: improving of Home page📦

--- fermacu/src/Pages/Home.jsx
+++ fermacu/src/Pages/Home.jsx
@@ -4,44 +4,28 @@ import React from "react";
 import { Grid } from "@mui/material";
 
 //Components
-import Dashboard from "../Components/HomeSections/Dashboard";
-import AboutMe from "../Components/HomeSections/AboutMe";
-import WorkWithMe from "../Components/HomeSections/WorkWithMe";
+import AboutMeSection from "../Components/HomeSections/AboutMeSection";
+import Services from "../Components/HomeSections/Services";
 
 //Helpers
-import {
-  typographyForSubtitles,
-} from "../Helpers/typography";
+import { typographyForSubtitles } from "../Helpers/typography";
 
 export default function Home() {
   // ==========> Component <==========
   return (
     <Grid container spacing={1} justifyContent="center">
-
-      {/**SECTION 1 - HOME */}
-      <Grid item xs={12} md={10}>
-        {typographyForSubtitles("> Home", "secondary")}
+      <Grid item xs={12} md={10} sx={{ marginTop: "15px" }}>
+        {typographyForSubtitles("Services", "secondary")}
       </Grid>
       <Grid item xs={12} md={10}>
-        <Dashboard />
+       <Services/> 
       </Grid>
-
-      {/**SECTION 2 - ABOUT ME */}
-      <Grid item xs={12} md={10} sx={{marginTop:"20px"}}>
-        {typographyForSubtitles("> About Me", "secondary")}
+      <Grid item xs={12} md={10} sx={{ marginTop: "15px" }}>
+        {typographyForSubtitles("About Me", "secondary")}
       </Grid>
       <Grid item xs={12} md={10}>
-        <AboutMe/>
+        <AboutMeSection />
       </Grid>
-
-      {/**SECTION 3 - WORK WITH ME*/}
-      <Grid item xs={12} md={10} sx={{marginTop:"20px"}}>
-        {typographyForSubtitles("> Have An Idea? Let's Build It.", "secondary")}
-      </Grid>
-      <Grid item xs={12} md={10}>
-        <WorkWithMe/>
-      </Grid>
-      
     </Grid>
   );
 }
