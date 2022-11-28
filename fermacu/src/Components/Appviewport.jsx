e0e2cc94a821a250d6bfbae4a243167359b77d52
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-27 21:33:12 -0600

    feat: navigation buttons in Appviewport✨

--- fermacu/src/Components/Appviewport.jsx
+++ fermacu/src/Components/Appviewport.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 
 //UI Material
-import { Grid } from "@mui/material";
+import { Button, Grid, Stack } from "@mui/material";
+import { ArrowForwardIosOutlined, GitHub, Instagram, LinkedIn, RocketLaunch } from "@mui/icons-material";
 
 //Helpers
 import LogoWhite from "./Logos/LogoWhite";
-import Home from "../Pages/Home";
 
-export default function Appviewport() {
+
+export default function Appviewport({page}) {
   return (
     <Grid container sx={{ padding: "15px", backgroundColor: "#000" }}>
       <Grid item xs={12} align="center">
         <LogoWhite />
       </Grid>
+      <Grid item xs={12} sx={{marginBottom:"20px"}}>
+        <Stack direction="row" spacing={1} justifyContent="center">
+          <Button
+            size="small"
+            startIcon={<RocketLaunch color="secondary" />}
+            endIcon={<ArrowForwardIosOutlined />}
+            variant="outlined"
+          >
+            EXPLORE
+          </Button>
+          <Button size="small" variant="outlined">
+            <Instagram color="secondary" />
+          </Button>
+          <Button size="small" variant="outlined">
+            <LinkedIn color="secondary" />
+          </Button>
+          <Button size="small" variant="outlined">
+            <GitHub color="secondary" />
+          </Button>
+        </Stack>
+      </Grid>
       <Grid item xs={12}>
-        <Home />
+        {page}
       </Grid>
     </Grid>
   );
