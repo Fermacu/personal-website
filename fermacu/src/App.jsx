61f3efe4a0f6d9c8fc2524d5fb6b7d0504864e5a
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-27 20:28:01 -0600

    refactor: improving of App📦

--- fermacu/src/App.jsx
+++ fermacu/src/App.jsx
@@ -9,26 +9,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Components
 import Appviewport from "./Components/Appviewport";
 
-<<<<<<< Updated upstream:fermacu/src/App.js
-=======
-//Pages
-import Home from "./Pages/Home"
-import Quidditch from "./Pages/Projects/Quidditch";
-import AboutMe from "./Pages/AboutMe";
-
->>>>>>> Stashed changes:fermacu/src/App.jsx
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-<<<<<<< Updated upstream:fermacu/src/App.js
           <Route path="/" element={<Appviewport/>}/>
-=======
-          <Route path="/" element={<Appviewport page={<Home/>}/>}/>
-          <Route path="/about" element={<Appviewport page={<AboutMe/>}/>}/>
-          <Route path="/quidditch" element={<Appviewport page={<Quidditch/>}/>}/>
->>>>>>> Stashed changes:fermacu/src/App.jsx
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
