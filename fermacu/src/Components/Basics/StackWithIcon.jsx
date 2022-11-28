9954087d5e84e3a9ea94dfd70d463a78adfb5e49
Fernando Matamoros <fer.andres@hotmail.com>
2022-11-27 22:05:50 -0600

    refactor: improving of StackWithIcon component📦

--- fermacu/src/Components/Basics/StackWithIcon.jsx
+++ fermacu/src/Components/Basics/StackWithIcon.jsx
@@ -6,14 +6,14 @@ import { Stack } from "@mui/material";
 //Helpers
 import { typographyForText } from "../../Helpers/typography";
 
-export default function StackWithIcon({ icon, title, text, text2 }) {
+export default function StackWithIcon({ icon, title, text, text2, color= "white" }) {
   return (
     <Stack direction="row" alignItems="center" spacing={2}>
       {icon}
       <Stack>
-      {typographyForText(title, "bold")}
-      {typographyForText(text)}
-      {typographyForText(text2, "bold")}
+      {typographyForText(title, "bold", color)}
+      {typographyForText(text, null, color)}
+      {typographyForText(text2, "bold", color)}
       </Stack>
     </Stack>
   );
